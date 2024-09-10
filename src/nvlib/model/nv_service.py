cf6e77da03e71a85f68882cdcdf0d0574e401683
peter88213 <petert88213@gmail.com>
2024-09-10 22:20:16 +0200

    Fix docstrings.

--- src/nvlib/model/nv_service.py
+++ src/nvlib/model/nv_service.py
@@ -1,7 +1,7 @@
 """Provide a class with getters and factory methods for novxlib objects.
 
 Copyright (c) 2024 Peter Triesberger
-For further information see https://github.com/peter88213/
+For further information see https://github.com/peter88213/novelibre
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from novxlib.config.configuration import Configuration
--- src/nvlib/view/icons.py
+++ src/nvlib/view/icons.py
@@ -1,7 +1,7 @@
 """Provide icons for the novelibre.GUI
 
 Copyright (c) 2024 Peter Triesberger
-For further information see https://github.com/peter88213/
+For further information see https://github.com/peter88213/novelibre
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 import os
