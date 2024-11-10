peter88213/novelibre

commit 5e0001db6930adf1192375aa14660ba4b607b8c3

Refactor
- Add NvView selected node/nodes properties.

src/nvlib/view/properties_window/full_section_view.py
@@ -435,7 +435,7 @@ class FullSectionView(DatedSectionView):
     def _add_plotline(self, event=None):
         # Add the selected element to the collection, if applicable.
         plotlineList = self._element.scPlotLines
-        plId = self._ui.tv.tree.selection()[0]
+        plId = self._ui.selectedNode
         if plId.startswith(PLOT_LINE_PREFIX) and not plId in plotlineList:
             plotlineList.append(plId)
             self._element.scPlotLines = plotlineList

src/nvlib/view/properties_window/plot_point_view.py
@@ -77,7 +77,7 @@ class TurningPointView(BasicView):
         
         End the picking mode after the section is assigned.
         """
-        nodeId = self._ui.tv.tree.selection()[0]
+        nodeId = self._ui.selectedNode
         if nodeId.startswith(SECTION_PREFIX):
             if self._mdl.novel.sections[nodeId].scType == 0:
                 self._clear_assignment()

src/nvlib/view/properties_window/related_section_view.py
@@ -211,7 +211,7 @@ class RelatedSectionView(BasicView):
     def _add_character(self, event=None):
         # Add the selected element to the collection, if applicable.
         crList = self._element.characters
-        crId = self._ui.tv.tree.selection()[0]
+        crId = self._ui.selectedNode
         if crId.startswith(CHARACTER_PREFIX) and not crId in crList:
             crList.append(crId)
             self._element.characters = crList
@@ -219,7 +219,7 @@ class RelatedSectionView(BasicView):
     def _add_location(self, event=None):
         # Add the selected element to the collection, if applicable.
         lcList = self._element.locations
-        lcId = self._ui.tv.tree.selection()[0]
+        lcId = self._ui.selectedNode
         if lcId.startswith(LOCATION_PREFIX)and not lcId in lcList:
             lcList.append(lcId)
             self._element.locations = lcList
@@ -227,7 +227,7 @@ class RelatedSectionView(BasicView):
     def _add_item(self, event=None):
         # Add the selected element to the collection, if applicable.
         itList = self._element.items
-        itId = self._ui.tv.tree.selection()[0]
+        itId = self._ui.selectedNode
         if itId.startswith(ITEM_PREFIX)and not itId in itList:
             itList.append(itId)
             self._element.items = itList

src/nvlib/controller/nv_controller.py
@@ -105,7 +105,7 @@ class NvController(ControllerBase):
         targetNode = kwargs.get('targetNode', None)
         if targetNode is None:
             try:
-                kwargs['targetNode'] = self._ui.tv.tree.selection()[0]
+                kwargs['targetNode'] = self._ui.selectedNode
             except:
                 pass
         newNode = self._mdl.add_chapter(**kwargs)
@@ -128,7 +128,7 @@ class NvController(ControllerBase):
         targetNode = kwargs.get('targetNode', None)
         if targetNode is None:
             try:
-                kwargs['targetNode'] = self._ui.tv.tree.selection()[0]
+                kwargs['targetNode'] = self._ui.selectedNode
             except:
                 pass
         newNode = self._mdl.add_character(**kwargs)
@@ -147,7 +147,7 @@ class NvController(ControllerBase):
             return
 
         try:
-            selection = self._ui.tv.tree.selection()[0]
+            selection = self._ui.selectedNode
         except:
             return
 
@@ -194,7 +194,7 @@ class NvController(ControllerBase):
             return
 
         try:
-            selection = self._ui.tv.tree.selection()[0]
+            selection = self._ui.selectedNode
         except:
             return
 
@@ -248,7 +248,7 @@ class NvController(ControllerBase):
         targetNode = kwargs.get('targetNode', None)
         if targetNode is None:
             try:
-                kwargs['targetNode'] = self._ui.tv.tree.selection()[0]
+                kwargs['targetNode'] = self._ui.selectedNode
             except:
                 pass
         newNode = self._mdl.add_item(**kwargs)
@@ -270,7 +270,7 @@ class NvController(ControllerBase):
         targetNode = kwargs.get('targetNode', None)
         if targetNode is None:
             try:
-                kwargs['targetNode'] = self._ui.tv.tree.selection()[0]
+                kwargs['targetNode'] = self._ui.selectedNode
             except:
                 pass
         newNode = self._mdl.add_location(**kwargs)
@@ -289,7 +289,7 @@ class NvController(ControllerBase):
             return
 
         try:
-            selection = self._ui.tv.tree.selection()[0]
+            selection = self._ui.selectedNode
         except:
             return
 
@@ -315,7 +315,7 @@ class NvController(ControllerBase):
         targetNode = kwargs.get('targetNode', None)
         if targetNode is None:
             try:
-                kwargs['targetNode'] = self._ui.tv.tree.selection()[0]
+                kwargs['targetNode'] = self._ui.selectedNode
             except:
                 pass
         newNode = self._mdl.add_part(**kwargs)
@@ -337,7 +337,7 @@ class NvController(ControllerBase):
         targetNode = kwargs.get('targetNode', None)
         if targetNode is None:
             try:
-                kwargs['targetNode'] = self._ui.tv.tree.selection()[0]
+                kwargs['targetNode'] = self._ui.selectedNode
             except:
                 pass
         newNode = self._mdl.add_plot_line(**kwargs)
@@ -359,7 +359,7 @@ class NvController(ControllerBase):
         targetNode = kwargs.get('targetNode', None)
         if targetNode is None:
             try:
-                kwargs['targetNode'] = self._ui.tv.tree.selection()[0]
+                kwargs['targetNode'] = self._ui.selectedNode
             except:
                 pass
         newNode = self._mdl.add_plot_point(**kwargs)
@@ -378,7 +378,7 @@ class NvController(ControllerBase):
         targetNode = kwargs.get('targetNode', None)
         if targetNode is None:
             try:
-                kwargs['targetNode'] = self._ui.tv.tree.selection()[0]
+                kwargs['targetNode'] = self._ui.selectedNode
             except:
                 pass
         newNode = self._mdl.add_project_note(**kwargs)
@@ -408,7 +408,7 @@ class NvController(ControllerBase):
         targetNode = kwargs.get('targetNode', None)
         if targetNode is None:
             try:
-                kwargs['targetNode'] = self._ui.tv.tree.selection()[0]
+                kwargs['targetNode'] = self._ui.selectedNode
             except:
                 pass
         newNode = self._mdl.add_section(**kwargs)
@@ -432,7 +432,7 @@ class NvController(ControllerBase):
         targetNode = kwargs.get('targetNode', None)
         if targetNode is None:
             try:
-                kwargs['targetNode'] = self._ui.tv.tree.selection()[0]
+                kwargs['targetNode'] = self._ui.selectedNode
             except:
                 pass
         newNode = self._mdl.add_stage(**kwargs)
@@ -506,7 +506,7 @@ class NvController(ControllerBase):
 
         if elements is None:
             try:
-                elements = self._ui.tv.tree.selection()
+                elements = self._ui.selectedNodes
             except:
                 return
 
@@ -664,7 +664,7 @@ class NvController(ControllerBase):
 
         if scId0 is None or scId1 is None:
             try:
-                scId1 = self._ui.tv.tree.selection()[0]
+                scId1 = self._ui.selectedNode
             except:
                 return
 
@@ -1052,7 +1052,7 @@ class NvController(ControllerBase):
 
         if elemIds is None:
             try:
-                elemIds = self._ui.tv.tree.selection()
+                elemIds = self._ui.selectedNodes
             except:
                 return
 
@@ -1071,7 +1071,7 @@ class NvController(ControllerBase):
 
         if elemIds is None:
             try:
-                elemIds = self._ui.tv.tree.selection()
+                elemIds = self._ui.selectedNodes
             except:
                 return
 
@@ -1089,7 +1089,7 @@ class NvController(ControllerBase):
 
         if elemIds is None:
             try:
-                elemIds = self._ui.tv.tree.selection()
+                elemIds = self._ui.selectedNodes
             except:
                 return
 
@@ -1108,7 +1108,7 @@ class NvController(ControllerBase):
 
         if elemIds is None:
             try:
-                elemIds = self._ui.tv.tree.selection()
+                elemIds = self._ui.selectedNodes
             except:
                 return
 

src/nvlib/view/nv_view.py
@@ -108,6 +108,7 @@ class NvView(ViewBase, ControllerNode):
         self._mdl.add_observer(self.tv)
         self.register_client(self.tv)
         self.tv.pack(expand=True, fill='both')
+        self._selection = None
 
         #--- Middle frame (intended for the content viewer).
         self.middleFrame = ttk.Frame(self.appWindow, width=prefs['middle_frame_width'])
@@ -145,6 +146,14 @@ class NvView(ViewBase, ControllerNode):
 
         #--- tk root event bindings.
         self._bind_events()
+
+    @property
+    def selectedNode(self):
+        return self._selection[0]
+
+    @property
+    def selectedNodes(self):
+        return self._selection
 
     def detach_properties_frame(self, event=None):
         """View the properties in its own window."""
@@ -306,8 +315,9 @@ class NvView(ViewBase, ControllerNode):
     def on_change_selection(self, nodeId):
         """Event handler for element selection.
         
-        Show the properties of the selected element.
+        Show the properties/contents of the selected element.
         """
+        self._selection = self.tv.tree.selection()
         self.propertiesView.show_properties(nodeId)
         self.contentsView.see(nodeId)
 

src/nvlib/view/properties_window/basic_view.py
@@ -283,7 +283,7 @@ class BasicView(SubController, Observer, ttk.Frame):
 
     def _load_next(self):
         """Load the next tree element of the same type."""
-        thisNode = self._ui.tv.tree.selection()[0]
+        thisNode = self._ui.selectedNode
         nextNode = self._ui.tv.next_node(thisNode)
         if nextNode:
             self._ui.tv.see_node(nextNode)
@@ -291,7 +291,7 @@ class BasicView(SubController, Observer, ttk.Frame):
 
     def _load_prev(self):
         """Load the next tree element of the same type."""
-        thisNode = self._ui.tv.tree.selection()[0]
+        thisNode = self._ui.selectedNode
         prevNode = self._ui.tv.prev_node(thisNode)
         if prevNode:
             self._ui.tv.see_node(prevNode)
@@ -344,7 +344,7 @@ class BasicView(SubController, Observer, ttk.Frame):
         """
         self._pickCommand = command
         if not self._pickingMode:
-            self._lastSelected = self._ui.tv.tree.selection()[0]
+            self._lastSelected = self._ui.selectedNode
             self._ui.tv.config(cursor='plus')
             self._ui.tv.open_children('')
             self._uiEscBinding = self._ui.root.bind('<Escape>')

commit 621170e1f171459c8fd286b957c356fba89430cc

Fix a regression from 5e0001d where related elements are not added when selected in picking mode.

src/nvlib/view/properties_window/full_section_view.py
@@ -435,7 +435,7 @@ class FullSectionView(DatedSectionView):
     def _add_plotline(self, event=None):
         # Add the selected element to the collection, if applicable.
         plotlineList = self._element.scPlotLines
-        plId = self._ui.selectedNode
+        plId = self._ui.tv.tree.selection()[0]
         if plId.startswith(PLOT_LINE_PREFIX) and not plId in plotlineList:
             plotlineList.append(plId)
             self._element.scPlotLines = plotlineList

src/nvlib/view/properties_window/plot_point_view.py
@@ -77,7 +77,7 @@ class TurningPointView(BasicView):
         
         End the picking mode after the section is assigned.
         """
-        nodeId = self._ui.selectedNode
+        nodeId = self._ui.tv.tree.selection()[0]
         if nodeId.startswith(SECTION_PREFIX):
             if self._mdl.novel.sections[nodeId].scType == 0:
                 self._clear_assignment()

src/nvlib/view/properties_window/related_section_view.py
@@ -211,7 +211,7 @@ class RelatedSectionView(BasicView):
     def _add_character(self, event=None):
         # Add the selected element to the collection, if applicable.
         crList = self._element.characters
-        crId = self._ui.selectedNode
+        crId = self._ui.tv.tree.selection()[0]
         if crId.startswith(CHARACTER_PREFIX) and not crId in crList:
             crList.append(crId)
             self._element.characters = crList
@@ -219,7 +219,7 @@ class RelatedSectionView(BasicView):
     def _add_location(self, event=None):
         # Add the selected element to the collection, if applicable.
         lcList = self._element.locations
-        lcId = self._ui.selectedNode
+        lcId = self._ui.tv.tree.selection()[0]
         if lcId.startswith(LOCATION_PREFIX)and not lcId in lcList:
             lcList.append(lcId)
             self._element.locations = lcList
@@ -227,7 +227,7 @@ class RelatedSectionView(BasicView):
     def _add_item(self, event=None):
         # Add the selected element to the collection, if applicable.
         itList = self._element.items
-        itId = self._ui.selectedNode
+        itId = self._ui.tv.tree.selection()[0]
         if itId.startswith(ITEM_PREFIX)and not itId in itList:
             itList.append(itId)
             self._element.items = itList