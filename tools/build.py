5f62913b6b5b1a6086e32f766b45af7237a8e4ab
peter88213 <petert88213@gmail.com>
2024-11-28 00:10:27 +0100

    Version 5.0.10
    Bugfix:
    
    - Move the importFiletypes instance variable from MainController to
    FileManager.

--- tools/build.py
+++ tools/build.py
@@ -12,7 +12,7 @@ import sys
 sys.path.insert(0, f'{os.getcwd()}/../../novelibre/tools')
 from package_builder import PackageBuilder
 
-VERSION = '5.0.9'
+VERSION = '5.0.10'
 
 
 class ApplicationBuilder(PackageBuilder):
