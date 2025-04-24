79a358bb6a5c4a7cabfaabc4658e071ae8447924
peter88213 <petert88213@gmail.com>
2025-04-24 12:17:26 +0200

    Providing a service method to get the final document export class (#59):

--- src/nvlib/controller/services/nv_service.py
+++ src/nvlib/controller/services/nv_service.py
@@ -4,15 +4,20 @@ Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/novelibre
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
+
 from nvlib.configuration.configuration import Configuration
 from nvlib.controller.services.novx_service import NovxService
 from nvlib.model.data.moon import Moon
 from nvlib.model.data.novel import Novel
 from nvlib.model.nv_treeview import NvTreeview
+from nvlib.model.odt.odt_w_export import OdtWExport
 
 
 class NvService(NovxService):
     """Getters and factory methods for nvlib model objects."""
+
+    def final_document_class(self):
+        return OdtWExport
 
     def get_moon_phase_str(self, isoDate):
         return Moon.get_phase_string(isoDate)
