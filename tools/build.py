2540f03c2f3691b0e3545d3fbe5eab67a77683e1
peter88213 <petert88213@gmail.com>
2025-05-25 13:47:57 +0200

    Version 5.23.5
    - Changed icon colors for better display in dark mode.

--- tools/build.py
+++ tools/build.py
@@ -12,7 +12,7 @@ import sys
 sys.path.insert(0, f'{os.getcwd()}/../../novelibre/tools')
 from package_builder import PackageBuilder
 
-VERSION = '5.23.4'
+VERSION = '5.23.5'
 
 
 class ApplicationBuilder(PackageBuilder):
