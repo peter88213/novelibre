35bb4b89b2db2600b9bb48efb6b81c0ea91e2fac
peter88213 <petert88213@gmail.com>
2025-06-11 14:16:22 +0200

    Fixed the PyCalendar unit tests.

--- test/test_character_age.py
+++ test/test_character_age.py
@@ -11,27 +11,50 @@ from nvlib.model.data.py_calendar import PyCalendar
 class NrmOpr(unittest.TestCase):
 
     def testBirthday(self):
-        self.assertEqual(PyCalendar.age('2015-09-08', '1989-09-08', '2018-04-20'), 26)
-        self.assertEqual(PyCalendar.age('2016-09-08', '1989-09-08', '2019-04-20'), 27)
-        self.assertEqual(PyCalendar.age('2016-02-01', '1989-02-01', '2019-04-20'), 27)
+        self.assertEqual(PyCalendar.age(
+            '2015-09-08', '1989-09-08', '2018-04-20'),
+            (26, None, 9496, None))
+        self.assertEqual(PyCalendar.age(
+            '2016-09-08', '1989-09-08', '2019-04-20'),
+            (27, None, 9862, None))
+        self.assertEqual(PyCalendar.age(
+            '2016-02-01', '1989-02-01', '2019-04-20'),
+            (27, None, 9861, None))
 
     def testOneDayBefore(self):
-        self.assertEqual(PyCalendar.age('2015-09-07', '1989-09-08', '2018-04-20'), 25)
-        self.assertEqual(PyCalendar.age('2016-09-07', '1989-09-08', '2019-04-20'), 26)
-        self.assertEqual(PyCalendar.age('2016-01-31', '1989-02-01', '2019-04-20'), 26)
+        self.assertEqual(PyCalendar.age(
+            '2015-09-07', '1989-09-08', '2018-04-20'),
+            (25, None, 9495, None))
+        self.assertEqual(PyCalendar.age(
+            '2016-09-07', '1989-09-08', '2019-04-20'),
+            (26, None, 9861, None))
+        self.assertEqual(PyCalendar.age(
+            '2016-01-31', '1989-02-01', '2019-04-20'),
+            (26, None, 9860, None))
 
     def testDead(self):
-        self.assertEqual(PyCalendar.age('2019-04-20', '1989-09-08', '2018-04-20'), -1)
-        self.assertEqual(PyCalendar.age('2028-04-20', '1989-09-08', '2018-04-20'), -10)
-        self.assertEqual(PyCalendar.age('2019-04-19', '1989-09-08', '2018-04-20'), 0)
+        self.assertEqual(PyCalendar.age(
+            '2019-04-20', '1989-09-08', '2018-04-20'),
+            (28, 1, None, 365))
+        self.assertEqual(PyCalendar.age(
+            '2028-04-20', '1989-09-08', '2018-04-20'),
+            (28, 10, None, 3653))
+        self.assertEqual(PyCalendar.age(
+            '2019-04-19', '1989-09-08', '2018-04-20'),
+            (28, 0, None, 364))
 
     def testMissing(self):
-        self.assertEqual(PyCalendar.age('2028-04-20', None, '2018-04-20'), -10)
+        self.assertEqual(PyCalendar.age(
+            '2028-04-20', None, '2018-04-20'),
+            (None, 10, None, 3653))
         # "now" is after the death date, so the birth date isn't needed
-        self.assertRaises(TypeError, lambda:PyCalendar.age('2015-09-08', None, '2018-04-20'))
+
+        self.assertRaises(UnboundLocalError, lambda:PyCalendar.age('2015-09-08', None, '2018-04-20'))
         # "now" is before the death date, so the birth date is needed
-        self.assertRaises(TypeError, lambda:PyCalendar.age('2016-09-08', None, None))
+
+        self.assertRaises(UnboundLocalError, lambda:PyCalendar.age('2016-09-08', None, None))
         # neither birth date nor death date is given
+
         self.assertRaises(TypeError, lambda:PyCalendar.age(None, '1989-02-01', '2019-04-20'))
         # no "Now" given
 
