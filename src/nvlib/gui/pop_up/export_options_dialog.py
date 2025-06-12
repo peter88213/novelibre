9cd144ee6cc73e65518e554da668474001f57a67
peter88213 <petert88213@gmail.com>
2025-06-12 10:33:06 +0200

    Refactored the export options dialog.

--- src/nvlib/gui/pop_up/export_options_dialog.py
+++ src/nvlib/gui/pop_up/export_options_dialog.py
@@ -7,14 +7,15 @@ License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 from tkinter import ttk
 
 from nvlib.gui.platform.platform_settings import KEYS
-from nvlib.gui.pop_up.export_options_ctrl import ExportOptionsCtrl
+from nvlib.controller.sub_controller import SubController
 from nvlib.gui.widgets.modal_dialog import ModalDialog
 from nvlib.nv_globals import prefs
 from nvlib.nv_locale import _
 import tkinter as tk
+from nvlib.controller.services.nv_help import NvHelp
 
 
-class ExportOptionsDialog(ModalDialog, ExportOptionsCtrl):
+class ExportOptionsDialog(ModalDialog, SubController):
     """A pop-up window with export preference settings."""
 
     def __init__(self, model, view, controller, **kw):
@@ -31,22 +32,22 @@ class ExportOptionsDialog(ModalDialog, ExportOptionsCtrl):
         frame1.pack(fill='both', padx=50)
 
         # Checkbox: Ask whether documents should be opened straight after export.
-        self.askDocOpenVar = tk.BooleanVar(frame1, value=prefs['ask_doc_open'])
+        self._askDocOpenVar = tk.BooleanVar(frame1, value=prefs['ask_doc_open'])
         ttk.Checkbutton(
             frame1,
             text=_('Ask before opening exported documents'),
-            variable=self.askDocOpenVar
+            variable=self._askDocOpenVar
             ).pack(padx=5, pady=5, anchor='w')
-        self.askDocOpenVar.trace('w', self.change_ask_doc_open)
+        self._askDocOpenVar.trace('w', self._change_ask_doc_open)
 
         # Checkbox: Lock the project after document export.
-        self.lockOnExportVar = tk.BooleanVar(frame1, value=prefs['lock_on_export'])
+        self._lockOnExportVar = tk.BooleanVar(frame1, value=prefs['lock_on_export'])
         ttk.Checkbutton(
             frame1,
             text=_('Lock the project after document export for editing'),
-            variable=self.lockOnExportVar
+            variable=self._lockOnExportVar
             ).pack(padx=5, pady=5, anchor='w')
-        self.lockOnExportVar.trace('w', self.change_lock_on_export)
+        self._lockOnExportVar.trace('w', self._change_lock_on_export)
 
         ttk.Separator(window, orient='horizontal').pack(fill='x')
 
@@ -80,9 +81,18 @@ class ExportOptionsDialog(ModalDialog, ExportOptionsCtrl):
         ttk.Button(
             self,
             text=_('Online help'),
-            command=self.open_help
+            command=self._open_help
             ).pack(padx=5, pady=5, side='right')
 
         # Set Key bindings.
-        self.bind(KEYS.OPEN_HELP[0], self.open_help)
+        self.bind(KEYS.OPEN_HELP[0], self._open_help)
+
+    def _change_ask_doc_open(self, *args):
+        prefs['ask_doc_open'] = self._askDocOpenVar.get()
+
+    def _change_lock_on_export(self, *args):
+        prefs['lock_on_export'] = self._lockOnExportVar.get()
+
+    def _open_help(self, event=None):
+        NvHelp.open_help_page(f'export_menu.html#{_("options").lower()}')
 
