peter88213/novelibre

commit 13b51c576181499bdc81dbea7402fcbcd3949cc0

Refactored
- Escaping the meta.xml author, title, and summary entries.
- Reformatted the code acc. to PEP-8.

src/nvlib/model/odf/odf_file.py
@@ -7,6 +7,7 @@ For further information see https://github.com/peter88213/novelibre
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from datetime import datetime
+from xml.sax.saxutils import escape
 import os
 from shutil import rmtree
 from string import Template
@@ -55,14 +56,18 @@ class OdfFile(FileExport):
         xhtml='http://www.w3.org/1999/xhtml',
         grddl='http://www.w3.org/2003/g/data-view#',
         tableooo='http://openoffice.org/2009/table',
-        loext='urn:org:documentfoundation:names:experimental:office:xmlns:loext:1.0'
+        loext=(
+            'urn:org:documentfoundation:names:experimental:'
+            'office:xmlns:loext:1.0'
+        ),
     )
 
     def __init__(self, filePath, **kwargs):
         """Create a temporary directory for zipfile generation.
         
         Positional arguments:
-            filePath: str -- path to the file represented by the Novel instance.
+            filePath: str -- path to the file 
+                             represented by the Novel instance.
             
         Optional arguments:
             kwargs -- keyword arguments to be used by subclasses.            
@@ -74,7 +79,8 @@ class OdfFile(FileExport):
         self._originalPath = self._filePath
 
     def __del__(self):
-        """Make sure to delete the temporary directory, in case write() has not been called."""
+        # Make sure to delete the temporary directory,
+        # in case write() has not been called.
         self._tear_down()
 
     def is_locked(self):
@@ -93,7 +99,8 @@ class OdfFile(FileExport):
         """
 
         #--- Create a temporary directory
-        # containing the internal structure of an ODS file except "content.xml".
+        # containing the internal structure of an ODS file
+        # except "content.xml".
         self._set_up()
 
         #--- Add "content.xml" to the temporary directory.
@@ -109,7 +116,12 @@ class OdfFile(FileExport):
             try:
                 os.replace(self.filePath, f'{self.filePath}.bak')
             except:
-                raise Error(f'{_("Cannot overwrite file")}: "{norm_path(self.filePath)}".')
+                raise Error(
+                    (
+                        f'{_("Cannot overwrite file")}: '
+                        f'"{norm_path(self.filePath)}".'
+                    )
+                )
             else:
                 backedUp = True
         try:
@@ -121,7 +133,12 @@ class OdfFile(FileExport):
             os.chdir(workdir)
             if backedUp:
                 os.replace(f'{self.filePath}.bak', self.filePath)
-            raise Error(f'{_("Cannot create file")}: "{norm_path(self.filePath)}".')
+            raise Error(
+                (
+                    f'{_("Cannot create file")}: '
+                    f'"{norm_path(self.filePath)}".'
+                )
+            )
 
         #--- Remove temporary data.
         os.chdir(workdir)
@@ -140,11 +157,10 @@ class OdfFile(FileExport):
         return stylesXmlStr
 
     def _set_up(self):
-        """Helper method for ZIP file generation.
-
-        Prepare the temporary directory containing the internal structure of an ODF file except 'content.xml'.
-        Raise the "Error" exception in case of error. 
-        """
+        # Helper method for ZIP file generation.
+        # Prepare the temporary directory containing the internal structure
+        # of an ODF file except 'content.xml'.
+        # Raise the "Error" exception in case of error.
 
         #--- Create and open a temporary directory for the files to zip.
         try:
@@ -152,25 +168,41 @@ class OdfFile(FileExport):
             os.mkdir(self._tempDir)
             os.mkdir(f'{self._tempDir}/META-INF')
         except:
-            raise Error(f'{_("Cannot create directory")}: "{norm_path(self._tempDir)}".')
-
+            raise Error(
+                (
+                    f'{_("Cannot create directory")}: '
+                    f'"{norm_path(self._tempDir)}".'
+                )
+            )
         #--- Generate mimetype.
         try:
-            with open(f'{self._tempDir}/mimetype', 'w', encoding='utf-8') as f:
+            with open(
+                f'{self._tempDir}/mimetype',
+                'w',
+                encoding='utf-8'
+            ) as f:
                 f.write(self._MIMETYPE)
         except:
             raise Error(f'{_("Cannot write file")}: "mimetype"')
 
         #--- Generate settings.xml.
         try:
-            with open(f'{self._tempDir}/settings.xml', 'w', encoding='utf-8') as f:
+            with open(
+                f'{self._tempDir}/settings.xml',
+                'w',
+                encoding='utf-8'
+            ) as f:
                 f.write(self._SETTINGS_XML)
         except:
             raise Error(f'{_("Cannot write file")}: "settings.xml"')
 
         #--- Generate META-INF\manifest.xml.
         try:
-            with open(f'{self._tempDir}/META-INF/manifest.xml', 'w', encoding='utf-8') as f:
+            with open(
+                f'{self._tempDir}/META-INF/manifest.xml',
+                'w',
+                encoding='utf-8'
+            ) as f:
                 f.write(self._MANIFEST_XML)
         except:
             raise Error(f'{_("Cannot write file")}: "manifest.xml"')
@@ -178,28 +210,37 @@ class OdfFile(FileExport):
         #--- Generate styles.xml.
         stylesXmlStr = self._get_styles_xml_str()
         try:
-            with open(f'{self._tempDir}/styles.xml', 'w', encoding='utf-8') as f:
+            with open(
+                f'{self._tempDir}/styles.xml',
+                'w',
+                encoding='utf-8'
+            ) as f:
                 f.write(stylesXmlStr)
         except:
             raise Error(f'{_("Cannot write file")}: "styles.xml"')
 
         #--- Generate meta.xml with actual document metadata.
         metaMapping = dict(
-            Author=self.novel.authorName,
-            Title=self.novel.title,
-            Summary=f'<![CDATA[{self.novel.desc}]]>',
+            Author=escape(self.novel.authorName),
+            Title=escape(self.novel.title),
+            Summary=escape(self.novel.desc),
             Datetime=datetime.today().replace(microsecond=0).isoformat(),
         )
         template = Template(self._META_XML)
         stylesXmlStr = template.safe_substitute(metaMapping)
         try:
-            with open(f'{self._tempDir}/meta.xml', 'w', encoding='utf-8') as f:
+            with open(
+                f'{self._tempDir}/meta.xml',
+                'w',
+                encoding='utf-8'
+            ) as f:
                 f.write(stylesXmlStr)
         except:
             raise Error(f'{_("Cannot write file")}: "meta.xml".')
 
     def _tear_down(self):
-        """Delete the temporary directory containing the unpacked ODF directory structure."""
+        # Delete the temporary directory containing the
+        # unpacked ODF directory structure.
         try:
             rmtree(self._tempDir)
         except:

src/nvlib/model/odt/odt_w_brief_synopsis.py
@@ -17,17 +17,20 @@ class OdtWBriefSynopsis(OdtWriter):
     DESCRIPTION = _('Brief synopsis')
     SUFFIX = BRF_SYNOPSIS_SUFFIX
 
-    _fileHeader = f'''{OdtWriter._CONTENT_XML_HEADER}<text:p text:style-name="Title">$Title</text:p>
-<text:p text:style-name="Subtitle">$AuthorName</text:p>$Filters
-'''
-
-    _partTemplate = '''<text:h text:style-name="Heading_20_1" text:outline-level="1">$Title</text:h>
-'''
-
-    _chapterTemplate = '''<text:h text:style-name="Heading_20_2" text:outline-level="2">$Title</text:h>
-'''
-
-    _sectionTemplate = '''<text:p text:style-name="Text_20_body">$Title</text:p>
-'''
-
+    _fileHeader = (
+        f'{OdtWriter._CONTENT_XML_HEADER}'
+        '<text:p text:style-name="Title">$Title</text:p>\n'
+        '<text:p text:style-name="Subtitle">$AuthorName</text:p>$Filters\n'
+    )
+    _partTemplate = (
+        '<text:h text:style-name="Heading_20_1" text:outline-level="1">'
+        '$Title</text:h>\n'
+    )
+    _chapterTemplate = (
+        '<text:h text:style-name="Heading_20_2" text:outline-level="2">'
+        '$Title</text:h>\n'
+    )
+    _sectionTemplate = (
+        '<text:p text:style-name="Text_20_body">$Title</text:p>\n'
+    )
     _fileFooter = OdtWriter._CONTENT_XML_FOOTER

src/nvlib/model/odt/odt_w_chapterdesc.py
@@ -17,17 +17,21 @@ class OdtWChapterDesc(OdtWriter):
     DESCRIPTION = _('Chapter descriptions')
     SUFFIX = CHAPTERS_SUFFIX
 
-    _fileHeader = f'''{OdtWriter._CONTENT_XML_HEADER}<text:p text:style-name="Title">$Title</text:p>
-<text:p text:style-name="Subtitle">$AuthorName</text:p>$Filters
-'''
-
-    _partTemplate = '''<text:h text:style-name="Heading_20_1" text:outline-level="1">$Title</text:h>
-'''
-
-    _chapterTemplate = '''<text:section text:style-name="Sect1" text:name="$ID">
-<text:h text:style-name="Heading_20_2" text:outline-level="2"><text:a xlink:href="../$ProjectName$ManuscriptSuffix.odt#$Title|outline">$Title</text:a></text:h>
-$Desc
-</text:section>
-'''
-
+    _fileHeader = (
+        f'{OdtWriter._CONTENT_XML_HEADER}'
+        '<text:p text:style-name="Title">$Title</text:p>\n'
+        '<text:p text:style-name="Subtitle">$AuthorName</text:p>$Filters\n'
+    )
+    _partTemplate = (
+        '<text:h text:style-name="Heading_20_1" text:outline-level="1">'
+        '$Title</text:h>\n'
+    )
+    _chapterTemplate = (
+        '<text:section text:style-name="Sect1" text:name="$ID">\n'
+        '<text:h text:style-name="Heading_20_2" text:outline-level="2">'
+        '<text:a xlink:href="../$ProjectName$ManuscriptSuffix.odt#'
+        '$Title|outline">$Title</text:a></text:h>\n'
+        '$Desc\n'
+        '</text:section>\n'
+    )
     _fileFooter = OdtWriter._CONTENT_XML_FOOTER

src/nvlib/model/odt/odt_w_characters.py
@@ -17,31 +17,37 @@ class OdtWCharacters(OdtWriter):
     DESCRIPTION = _('Character descriptions')
     SUFFIX = CHARACTERS_SUFFIX
 
-    _fileHeader = f'''{OdtWriter._CONTENT_XML_HEADER}<text:p text:style-name="Title">$Title</text:p>
-<text:p text:style-name="Subtitle">$AuthorName</text:p>$Filters
-'''
-
-    _characterTemplate = f'''<text:h text:style-name="Heading_20_2" text:outline-level="2">$Title$FullName$AKA</text:h>
-<text:section text:style-name="Sect1" text:name="$ID">
-<text:h text:style-name="Heading_20_3" text:outline-level="3">{_("Description")}</text:h>
-<text:section text:style-name="Sect1" text:name="desc:$ID">
-$Desc
-</text:section>
-<text:h text:style-name="Heading_20_3" text:outline-level="3">$CustomChrBio</text:h>
-<text:section text:style-name="Sect1" text:name="bio:$ID">
-$Bio
-</text:section>
-<text:h text:style-name="Heading_20_3" text:outline-level="3">$CustomChrGoals</text:h>
-<text:section text:style-name="Sect1" text:name="goals:$ID">
-$Goals
-</text:section>
-<text:h text:style-name="Heading_20_3" text:outline-level="3">{_("Notes")}</text:h>
-<text:section text:style-name="Sect1" text:name="notes:$ID">
-$Notes
-</text:section>
-</text:section>
-'''
-
+    _fileHeader = (
+        f'{OdtWriter._CONTENT_XML_HEADER}'
+        '<text:p text:style-name="Title">$Title</text:p>\n'
+        '<text:p text:style-name="Subtitle">$AuthorName</text:p>$Filters\n'
+    )
+    _characterTemplate = (
+        '<text:h text:style-name="Heading_20_2" text:outline-level="2">'
+        '$Title$FullName$AKA</text:h>\n'
+        '<text:section text:style-name="Sect1" text:name="$ID">\n'
+        '<text:h text:style-name="Heading_20_3" text:outline-level="3">'
+        f'{_("Description")}</text:h>\n'
+        '<text:section text:style-name="Sect1" text:name="desc:$ID">\n'
+        '$Desc\n'
+        '</text:section>\n'
+        '<text:h text:style-name="Heading_20_3" text:outline-level="3">'
+        '$CustomChrBio</text:h>\n'
+        '<text:section text:style-name="Sect1" text:name="bio:$ID">\n'
+        '$Bio\n'
+        '</text:section>\n'
+        '<text:h text:style-name="Heading_20_3" text:outline-level="3">'
+        '$CustomChrGoals</text:h>\n'
+        '<text:section text:style-name="Sect1" text:name="goals:$ID">\n'
+        '$Goals\n'
+        '</text:section>\n'
+        '<text:h text:style-name="Heading_20_3" text:outline-level="3">'
+        f'{_("Notes")}</text:h>\n'
+        '<text:section text:style-name="Sect1" text:name="notes:$ID">\n'
+        '$Notes\n'
+        '</text:section>\n'
+        '</text:section>\n'
+    )
     _fileFooter = OdtWriter._CONTENT_XML_FOOTER
 
     def _get_characterMapping(self, crId):

src/nvlib/model/odt/odt_w_export.py
@@ -14,18 +14,21 @@ class OdtWExport(OdtWFormatted):
     Export a non-reimportable manuscript with chapters and sections.
     """
     DESCRIPTION = _('manuscript')
-    _fileHeader = f'''$ContentHeader<text:p text:style-name="Title">$Title</text:p>
-<text:p text:style-name="Subtitle">$AuthorName</text:p>$Filters
-'''
 
-    _partTemplate = '''<text:h text:style-name="Heading_20_1" text:outline-level="1">$Title</text:h>$Epigraph$EpigraphSrc
-'''
-
-    _chapterTemplate = '''<text:h text:style-name="Heading_20_2" text:outline-level="2">$Title</text:h>$Epigraph$EpigraphSrc
-'''
-
-    _sectionTemplate = '''$SectionContent
-'''
+    _fileHeader = (
+        '$ContentHeader'
+        '<text:p text:style-name="Title">$Title</text:p>\n'
+        '<text:p text:style-name="Subtitle">$AuthorName</text:p>$Filters\n'
+    )
+    _partTemplate = (
+        '<text:h text:style-name="Heading_20_1" text:outline-level="1">'
+        '$Title</text:h>$Epigraph$EpigraphSrc\n'
+    )
+    _chapterTemplate = (
+        '<text:h text:style-name="Heading_20_2" text:outline-level="2">'
+        '$Title</text:h>$Epigraph$EpigraphSrc\n'
+    )
+    _sectionTemplate = '$SectionContent\n'
 
     _sectionDivider = '<text:p text:style-name="Heading_20_4">* * *</text:p>\n'
     _fileFooter = OdtWFormatted._CONTENT_XML_FOOTER

src/nvlib/model/odt/odt_w_manuscript.py
@@ -17,22 +17,28 @@ class OdtWManuscript(OdtWFormatted):
     DESCRIPTION = _('Editable manuscript')
     SUFFIX = MANUSCRIPT_SUFFIX
 
-    _fileHeader = f'''$ContentHeader<text:p text:style-name="Title">$Title</text:p>
-<text:p text:style-name="Subtitle">$AuthorName</text:p>$Filters
-'''
-
-    _partTemplate = '''<text:h text:style-name="Heading_20_1" text:outline-level="1">$Title</text:h>$Epigraph$EpigraphSrc
-'''
-
-    _chapterTemplate = '''<text:h text:style-name="Heading_20_2" text:outline-level="2">$Title</text:h>$Epigraph$EpigraphSrc
-'''
-
-    _sectionTemplate = f'''<text:h text:style-name="{_('Heading_20_3_20_invisible')}" text:outline-level="3">$Title</text:h>
-<text:section text:style-name="Sect1" text:name="$ID">
-$SectionContent
-</text:section>
-'''
-    _sectionDivider = '<text:p text:style-name="Heading_20_4">* * *</text:p>\n'
-
+    _fileHeader = (
+        '$ContentHeader'
+        '<text:p text:style-name="Title">$Title</text:p>'
+        '\n<text:p text:style-name="Subtitle">$AuthorName</text:p>$Filters\n'
+    )
+    _partTemplate = (
+        '<text:h text:style-name="Heading_20_1" text:outline-level="1">'
+        '$Title</text:h>$Epigraph$EpigraphSrc\n'
+    )
+    _chapterTemplate = (
+        '<text:h text:style-name="Heading_20_2" text:outline-level="2">'
+        '$Title</text:h>$Epigraph$EpigraphSrc\n'
+    )
+    _sectionTemplate = (
+        f'<text:h text:style-name="{_("Heading_20_3_20_invisible")}" '
+        'text:outline-level="3">$Title</text:h>\n'
+        '<text:section text:style-name="Sect1" text:name="$ID">\n'
+        '$SectionContent\n'
+        '</text:section>\n'
+    )
+    _sectionDivider = (
+        '<text:p text:style-name="Heading_20_4">* * *</text:p>\n'
+    )
     _fileFooter = OdtWFormatted._CONTENT_XML_FOOTER
 

commit 953e48ea347e8a3fbf74d1c004ba8977c605a8b9

Refactored; updated coding conventions
- Fixed a regression from 13b51c5 where trying to escape None raises an
exception.
- Reformatted the code acc. to PEP-8.

src/nvlib/gui/tree_window/tree_viewer.py
@@ -1504,7 +1504,8 @@ class TreeViewer(ttk.Frame, Observer, SubController):
     def _update_node_values(self, nodeId, collect=False):
         # Add/remove node values collected from the node's children.
         # nodeId: str -- Node ID.
-        # collect: bool -- If True, add the collected values; if False, remove them.
+        # collect: bool -- If True, add the collected values;
+        #                  if False, remove them.
         if nodeId.startswith(CHAPTER_PREFIX):
             positionStr = self.tree.item(nodeId)['values'][self._colPos['po']]
             __, nodeValues, __ = self._get_chapter_row_data(

src/nvlib/model/odf/odf_file.py
@@ -145,6 +145,13 @@ class OdfFile(FileExport):
         self._tear_down()
         return f'{_("File written")}: "{norm_path(self.filePath)}".'
 
+    def _escape(self, text):
+        try:
+            return escape(text)
+
+        except AttributeError:
+            return text
+
     def _get_styles_xml_str(self):
         """Return the styles.xml data as a string."""
         self.novel.check_locale()
@@ -221,9 +228,9 @@ class OdfFile(FileExport):
 
         #--- Generate meta.xml with actual document metadata.
         metaMapping = dict(
-            Author=escape(self.novel.authorName),
-            Title=escape(self.novel.title),
-            Summary=escape(self.novel.desc),
+            Author=self._escape(self.novel.authorName),
+            Title=self._escape(self.novel.title),
+            Summary=self._escape(self.novel.desc),
             Datetime=datetime.today().replace(microsecond=0).isoformat(),
         )
         template = Template(self._META_XML)

src/nvlib/model/odt/odt_w_formatted.py
@@ -45,11 +45,19 @@ class OdtWFormatted(OdtWriter):
                 except:
                     lngCode = 'zxx'
                     ctrCode = 'none'
-                lines.append((f'  <style:style style:name="T{i}" style:family="text">\n'
-                              f'   <style:text-properties fo:language="{lngCode}" fo:country="{ctrCode}" '
-                              f'style:language-asian="{lngCode}" style:country-asian="{ctrCode}" '
-                              f'style:language-complex="{lngCode}" style:country-complex="{ctrCode}"/>\n'
-                              '  </style:style>'))
+                lines.append(
+                    (
+                        f'  <style:style style:name="T{i}" '
+                        'style:family="text">\n'
+                        f'   <style:text-properties '
+                        f'fo:language="{lngCode}" fo:country="{ctrCode}" '
+                        f'style:language-asian="{lngCode}" '
+                        f'style:country-asian="{ctrCode}" '
+                        f'style:language-complex="{lngCode}" '
+                        f'style:country-complex="{ctrCode}"/>\n'
+                        '  </style:style>'
+                    )
+                )
             lines.append(' </office:automatic-styles>')
             styleMapping['automaticStyles'] = '\n'.join(lines)
         else: