6d9dcc3c9da7279b105469aa6c486f4bf0a02dfb
peter88213 <petert88213@gmail.com>
2025-07-08 20:03:52 +0200

    Bugfix.

--- src/nvlib/model/novx/zipped_novx_file.py
+++ src/nvlib/model/novx/zipped_novx_file.py
@@ -6,6 +6,7 @@ License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from nvlib.model.novx.novx_file import NovxFile
 from nvlib.model.novx.zipped_novx_opener import ZippedNovxOpener
+from nvlib.nv_locale import _
 
 
 class ZippedNovxFile(NovxFile):
