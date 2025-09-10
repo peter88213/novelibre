2129c2f90796071b23a9590d79d1970c955f9990
peter88213 <petert88213@gmail.com>
2025-09-10 20:37:34 +0200

    Reformatted the code.

--- src/nvlib/gui/contents_window/rich_text_nv.py
+++ src/nvlib/gui/contents_window/rich_text_nv.py
@@ -32,18 +32,18 @@ class RichTextNv(RichTextTk):
         
         Extends the supeclass constructor
         """
-        super().__init__(*args,
-                height=20,
-                width=60,
-                spacing1=10,
-                spacing2=2,
-                wrap='word',
-                padx=10,
-                bg=kwargs['color_text_bg'],
-                fg=kwargs['color_text_fg'],
-                )
+        super().__init__(
+            *args,
+            height=20,
+            width=60,
+            spacing1=10,
+            spacing2=2,
+            wrap='word',
+            padx=10,
+            bg=kwargs['color_text_bg'],
+            fg=kwargs['color_text_fg'],
+        )
         defaultFont = tkFont.nametofont(self.cget('font'))
-
         defaultSize = defaultFont.cget('size')
         boldFont = tkFont.Font(**defaultFont.configure())
         italicFont = tkFont.Font(**defaultFont.configure())
@@ -53,98 +53,118 @@ class RichTextNv(RichTextTk):
 
         boldFont.configure(weight='bold')
         italicFont.configure(slant='italic')
-        h1Font.configure(size=int(defaultSize * self.H1_SIZE),
-                         weight='bold',
-                         )
-        h2Font.configure(size=int(defaultSize * self.H2_SIZE),
-                         weight='bold',
-                         )
-        h3Font.configure(size=int(defaultSize * self.H3_SIZE),
-                         slant='italic',
-                         )
-        self.tag_configure(self.XML_TAG,
-                           foreground=kwargs['color_xml_tag'],
-                           )
-        self.tag_configure(self.EM_TAG,
-                           font=italicFont,
-                           )
-        self.tag_configure(self.STRONG_TAG,
-                           font=boldFont,
-                           )
-        self.tag_configure(self.COMMENT_TAG,
-                           background=kwargs['color_comment_tag'],
-                           )
-        self.tag_configure(self.COMMENT_XML_TAG,
-                           foreground=kwargs['color_xml_tag'],
-                           background=kwargs['color_comment_tag'],
-                           )
-        self.tag_configure(self.NOTE_TAG,
-                           background=kwargs['color_comment_tag'],
-                           )
-        self.tag_configure(self.NOTE_XML_TAG,
-                           foreground=kwargs['color_xml_tag'],
-                           background=kwargs['color_note_tag'],
-                           )
-        self.tag_configure(self.H1_TAG,
-                           font=h1Font,
-                           spacing3=defaultSize,
-                           foreground=kwargs['color_chapter'],
-                           justify='center',
-                           spacing1=defaultSize * self.H1_SPACING,
-                           )
-        self.tag_configure(self.H1_UNUSED_TAG,
-                           font=h1Font,
-                           spacing3=defaultSize,
-                           foreground=kwargs['color_unused'],
-                           justify='center',
-                           spacing1=defaultSize * self.H1_SPACING,
-                           )
-        self.tag_configure(self.H2_TAG,
-                           font=h2Font,
-                           spacing3=defaultSize,
-                           foreground=kwargs['color_chapter'],
-                           justify='center',
-                           spacing1=defaultSize * self.H2_SPACING,
-                           )
-        self.tag_configure(self.H2_UNUSED_TAG,
-                           font=h2Font,
-                           spacing3=defaultSize,
-                           foreground=kwargs['color_unused'],
-                           justify='center',
-                           spacing1=defaultSize * self.H2_SPACING,
-                           )
-        self.tag_configure(self.H3_UNUSED_TAG,
-                           font=h3Font,
-                           spacing3=defaultSize,
-                           foreground=kwargs['color_unused'],
-                           justify='center',
-                           spacing1=defaultSize * self.H3_SPACING,
-                           )
-        self.tag_configure(self.EPIGRAPH_SRC_TAG,
-                           font=h3Font,
-                           spacing3=defaultSize,
-                           foreground=kwargs['color_epigraph'],
-                           justify='center',
-                           spacing1=defaultSize * self.H3_SPACING,
-                           )
-        self.tag_configure(self.UNUSED_TAG,
-                           foreground=kwargs['color_unused'],
-                           )
-        self.tag_configure(self.EPIGRAPH_TAG,
-                           foreground=kwargs['color_epigraph'],
-                           )
-        self.tag_configure(self.STAGE1_TAG,
-                           font=h1Font,
-                           spacing3=defaultSize,
-                           foreground=kwargs['color_stage'],
-                           justify='center',
-                           spacing1=defaultSize * self.H1_SPACING,
-                           )
-        self.tag_configure(self.STAGE2_TAG,
-                           font=h3Font,
-                           spacing3=defaultSize,
-                           foreground=kwargs['color_stage'],
-                           justify='center',
-                           spacing1=defaultSize * self.H3_SPACING,
-                           )
+        h1Font.configure(
+            size=int(defaultSize * self.H1_SIZE),
+            weight='bold',
+        )
+        h2Font.configure(
+            size=int(defaultSize * self.H2_SIZE),
+            weight='bold',
+        )
+        h3Font.configure(
+            size=int(defaultSize * self.H3_SIZE),
+            slant='italic',
+        )
+        self.tag_configure(
+            self.XML_TAG,
+            foreground=kwargs['color_xml_tag'],
+        )
+        self.tag_configure(
+            self.EM_TAG,
+            font=italicFont,
+        )
+        self.tag_configure(
+            self.STRONG_TAG,
+            font=boldFont,
+        )
+        self.tag_configure(
+            self.COMMENT_TAG,
+            background=kwargs['color_comment_tag'],
+        )
+        self.tag_configure(
+            self.COMMENT_XML_TAG,
+            foreground=kwargs['color_xml_tag'],
+            background=kwargs['color_comment_tag'],
+        )
+        self.tag_configure(
+            self.NOTE_TAG,
+            background=kwargs['color_comment_tag'],
+        )
+        self.tag_configure(
+            self.NOTE_XML_TAG,
+            foreground=kwargs['color_xml_tag'],
+            background=kwargs['color_note_tag'],
+        )
+        self.tag_configure(
+            self.H1_TAG,
+            font=h1Font,
+            spacing3=defaultSize,
+            foreground=kwargs['color_chapter'],
+            justify='center',
+            spacing1=defaultSize * self.H1_SPACING,
+        )
+        self.tag_configure(
+            self.H1_UNUSED_TAG,
+            font=h1Font,
+            spacing3=defaultSize,
+            foreground=kwargs['color_unused'],
+            justify='center',
+            spacing1=defaultSize * self.H1_SPACING,
+        )
+        self.tag_configure(
+            self.H2_TAG,
+            font=h2Font,
+            spacing3=defaultSize,
+            foreground=kwargs['color_chapter'],
+            justify='center',
+            spacing1=defaultSize * self.H2_SPACING,
+        )
+        self.tag_configure(
+            self.H2_UNUSED_TAG,
+            font=h2Font,
+            spacing3=defaultSize,
+            foreground=kwargs['color_unused'],
+            justify='center',
+            spacing1=defaultSize * self.H2_SPACING,
+        )
+        self.tag_configure(
+            self.H3_UNUSED_TAG,
+            font=h3Font,
+            spacing3=defaultSize,
+            foreground=kwargs['color_unused'],
+            justify='center',
+            spacing1=defaultSize * self.H3_SPACING,
+        )
+        self.tag_configure(
+            self.EPIGRAPH_SRC_TAG,
+            font=h3Font,
+            spacing3=defaultSize,
+            foreground=kwargs['color_epigraph'],
+            justify='center',
+            spacing1=defaultSize * self.H3_SPACING,
+        )
+        self.tag_configure(
+            self.UNUSED_TAG,
+            foreground=kwargs['color_unused'],
+        )
+        self.tag_configure(
+            self.EPIGRAPH_TAG,
+            foreground=kwargs['color_epigraph'],
+        )
+        self.tag_configure(
+            self.STAGE1_TAG,
+            font=h1Font,
+            spacing3=defaultSize,
+            foreground=kwargs['color_stage'],
+            justify='center',
+            spacing1=defaultSize * self.H1_SPACING,
+        )
+        self.tag_configure(
+            self.STAGE2_TAG,
+            font=h3Font,
+            spacing3=defaultSize,
+            foreground=kwargs['color_stage'],
+            justify='center',
+            spacing1=defaultSize * self.H3_SPACING,
+        )
 
