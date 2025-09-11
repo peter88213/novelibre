peter88213/novelibre

commit 8d1aaddc5dc24169f8cee17e254b15f993cc2751

Fixed a bug where languages aren't registered if only assigned to paragraphs.

src/nvlib/model/data/novel.py
@@ -11,7 +11,7 @@ from nvlib.model.data.basic_element import BasicElement
 from nvlib.model.data.py_calendar import PyCalendar
 import xml.etree.ElementTree as ET
 
-LANGUAGE_TAG = re.compile(r'\<span xml\:lang=\"(.*?)\"\>')
+LANGUAGE_TAG = re.compile(r'\<(p|span) xml\:lang=\"(.*?)\"\>')
 
 
 class Novel(BasicElement):
@@ -589,7 +589,7 @@ class Novel(BasicElement):
             m = LANGUAGE_TAG.search(text)
             while m:
                 text = text[m.span()[1]:]
-                yield m.group(1)
+                yield m.group(2)
                 m = LANGUAGE_TAG.search(text)
 
         self.languages = []

commit 208d1d201f37f6f435ee3ef63dccf8f1beb63582

Bugfix
- Fixed a regression from commit 8d1aadd where invalid auto-styles are
generated from "Quotations" paragraphs in foreign language.

src/nvlib/model/data/novel.py
@@ -11,7 +11,7 @@ from nvlib.model.data.basic_element import BasicElement
 from nvlib.model.data.py_calendar import PyCalendar
 import xml.etree.ElementTree as ET
 
-LANGUAGE_TAG = re.compile(r'\<(p|span) xml\:lang=\"(.*?)\"\>')
+LANGUAGE_TAG = re.compile(r'\<(p|span) xml\:lang=\"(.*?)\".*?\>')
 
 
 class Novel(BasicElement):
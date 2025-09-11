208d1d201f37f6f435ee3ef63dccf8f1beb63582
peter88213 <petert88213@gmail.com>
2025-09-11 21:41:01 +0200

    Bugfix
    - Fixed a regression from commit 8d1aadd where invalid auto-styles are
    generated from "Quotations" paragraphs in foreign language.

--- src/nvlib/model/data/novel.py
+++ src/nvlib/model/data/novel.py
@@ -11,7 +11,7 @@ from nvlib.model.data.basic_element import BasicElement
 from nvlib.model.data.py_calendar import PyCalendar
 import xml.etree.ElementTree as ET
 
-LANGUAGE_TAG = re.compile(r'\<(p|span) xml\:lang=\"(.*?)\"\>')
+LANGUAGE_TAG = re.compile(r'\<(p|span) xml\:lang=\"(.*?)\".*?\>')
 
 
 class Novel(BasicElement):
