af98c40f3befbd0151de112ebffedcde8c471140
peter88213 <petert88213@gmail.com>
2025-11-18 19:27:10 +0100

    Changed the "Online help" menu entry for the next novelibre version.

--- src/nvlib/gui/main_view.py
+++ src/nvlib/gui/main_view.py
@@ -953,7 +953,7 @@ class MainView(Observer, MsgBoxes, SubController):
             menu=self.helpMenu,
         )
         self.helpMenu.add_command(
-            label=_('Online user guide'),
+            label=_('Online help'),
             accelerator=KEYS.OPEN_HELP[1],
             command=self._ctrl.open_help,
         )
