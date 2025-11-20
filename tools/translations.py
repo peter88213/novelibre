a99e18c4901fa2bd73000183de5b2ab26d4eb7c0
peter88213 <petert88213@gmail.com>
2025-11-20 21:32:52 +0100

    Updated docstring.

--- tools/translations.py
+++ tools/translations.py
@@ -2,22 +2,6 @@
 
 Requires Python 3.9+
 
-File structure:
-
-├── novelibre/
-│   ├── i18n/
-│   │   ├── <language>.json
-│   │   ├── messages.pot
-│   │   └── <language>.po
-│   └── tools/
-│       └── translations.py
-└── <plugin>/
-    ├── i18n/ 
-    │   ├── messages.pot
-    │   └── <language>.po
-    └── tools/
-        └── <calling script>
-
 Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/novelibre
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
