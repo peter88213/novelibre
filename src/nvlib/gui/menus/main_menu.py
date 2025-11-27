37fd77fd84a45c5fe5aac539af576f6de79976d0
peter88213 <petert88213@gmail.com>
2025-11-27 13:17:45 +0100

    Removed unused import.

--- src/nvlib/gui/menus/main_menu.py
+++ src/nvlib/gui/menus/main_menu.py
@@ -5,7 +5,6 @@ For further information see https://github.com/peter88213/
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from nvlib.gui.menus.nv_menu import NvMenu
-from nvlib.gui.platform.platform_settings import KEYS
 from nvlib.nv_locale import _
 import tkinter as tk
 
