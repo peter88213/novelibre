63f96643895e7737da11f9da5aa9276330281043
peter88213 <petert88213@gmail.com>
2026-01-01 11:07:22 +0100

    Extended the regression tests.
    New:
    - Reimport story structure description.
    - Reimport plot line descriptions.

--- test/test_plotlines.py
+++ test/test_plotlines.py
@@ -5,18 +5,23 @@ Test the plot line descriptions generation.
 For further information see https://github.com/peter88213/novelibre
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
+from nvlib.model.odt.odt_r_plotlines import OdtRPlotlines
 from nvlib.model.odt.odt_w_plotlines import OdtWPlotlines
-from testlib.export_test import ExportTest
+from testlib.import_export_test import ImportExportTest
 import unittest
 
 
-class NrmOpr(ExportTest, unittest.TestCase):
+class NrmOpr(ImportExportTest, unittest.TestCase):
+    _importClass = OdtRPlotlines
     _exportClass = OdtWPlotlines
 
     # The test methods must be defined here to identify the source of failure.
 
     def test_novx_to_exp(self):
         super().test_novx_to_exp()
+
+    def test_imp_to_novx(self):
+        super().test_imp_to_novx()
 
 
 def main():
--- test/test_stages.py
+++ test/test_stages.py
@@ -5,18 +5,24 @@ Test the story structure description generation.
 For further information see https://github.com/peter88213/novelibre
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
-from nvlib.model.odt.odt_w_stages import OdtWStages
-from testlib.export_test import ExportTest
 import unittest
 
+from nvlib.model.odt.odt_r_stages import OdtRStages
+from nvlib.model.odt.odt_w_stages import OdtWStages
+from testlib.import_export_test import ImportExportTest
 
-class NrmOpr(ExportTest, unittest.TestCase):
+
+class NrmOpr(ImportExportTest, unittest.TestCase):
+    _importClass = OdtRStages
     _exportClass = OdtWStages
 
     # The test methods must be defined here to identify the source of failure.
 
     def test_novx_to_exp(self):
         super().test_novx_to_exp()
+
+    def test_imp_to_novx(self):
+        super().test_imp_to_novx()
 
 
 def main():
