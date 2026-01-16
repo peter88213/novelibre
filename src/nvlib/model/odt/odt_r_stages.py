63651db768b3a85e7c2c1506e904fa4e12e18cae
peter88213 <petert88213@gmail.com>
2026-01-16 13:15:40 +0100

    Reordered imports.

--- src/nvlib/model/odt/odt_r_stages.py
+++ src/nvlib/model/odt/odt_r_stages.py
@@ -4,10 +4,10 @@ Copyright (c) Peter Triesberger
 For further information see https://github.com/peter88213/novelibre
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
+from nvlib.model.data.stage_splitter import StageSplitter
 from nvlib.model.odt.odt_r_sectiondesc import OdtRSectionDesc
 from nvlib.novx_globals import STAGES_SUFFIX
 from nvlib.nv_locale import _
-from nvlib.model.data.stage_splitter import StageSplitter
 
 
 class OdtRStages(OdtRSectionDesc):
