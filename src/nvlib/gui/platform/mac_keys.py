767de65c7c8d3ac9c53951336368a380c515e390
peter88213 <petert88213@gmail.com>
2026-02-28 20:18:24 +0100

    Added missing import (#28).

--- src/nvlib/gui/platform/mac_keys.py
+++ src/nvlib/gui/platform/mac_keys.py
@@ -5,6 +5,7 @@ For further information see https://github.com/peter88213/novelibre
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
 """
 from nvlib.gui.platform.generic_keys import GenericKeys
+from nvlib.nv_locale import _
 
 
 class MacKeys(GenericKeys):
