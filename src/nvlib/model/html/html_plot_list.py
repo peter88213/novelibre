peter88213/novelibre

commit 53829dac82b38829d181e484f888244194245c60

Minor refactoring.

src/nvlib/model/html/html_plot_list.py
@@ -43,15 +43,22 @@ class HtmlPlotList(HtmlReport):
             else:
                 fgColor = DEFAULT_TEXT_COLOR
                 bgColor = DEFAULT_PLOTLINE_COLOR
+
+            # Plot line column heading cell style.
             htmlText.append(
                 f'td.h{plId} {{'
                 f'background: {bgColor}; '
-                f'color: {fgColor}}}'
+                f'color: {fgColor}'
+                '}}'
             )
+
+            # Plot line node cell style.
             htmlText.append(
-                f'td.{plId} {{border-left: 0.5em solid {bgColor}; '
+                f'td.{plId} {{'
+                f'border-left: 0.5em solid {bgColor}; '
                 f'background: {DEFAULT_PLOTLINE_COLOR}; '
-                f'color: {DEFAULT_TEXT_COLOR}}}'
+                f'color: {DEFAULT_TEXT_COLOR}'
+                '}}'
             )
         htmlText.append(
             '</style>\n'

src/nvlib/model/ods/ods_w_plot_list.py
@@ -108,15 +108,20 @@ class OdsWPlotList(OdsWriter):
         DEFAULT_TEXT_COLOR = BLACK = '#000000'
         WHITE = '#ffffff'
 
+        # Plot line column heading cell style.
         styleTemplateHeading = (
             '  <style:style style:name="h$Name" style:family="table-cell" '
             'style:parent-style-name="Default">\n'
-            '   <style:table-cell-properties fo:background-color="$BgColor"/>\n'
+            '   <style:table-cell-properties '
+            'fo:background-color="$BgColor"/>\n'
             '   <style:text-properties fo:color="$FgColor" '
-            'fo:font-weight="bold" style:font-weight-asian="bold" '
+            'fo:font-weight="bold" '
+            'style:font-weight-asian="bold" '
             'style:font-weight-complex="bold"/>\n'
             '  </style:style>'
         )
+
+        # Plot line node cell style.
         styleTemplate = (
             '  <style:style style:name="$Name" style:family="table-cell" '
             'style:parent-style-name="Default">\n'
@@ -128,6 +133,7 @@ class OdsWPlotList(OdsWriter):
             '   <style:text-properties fo:color="$DefaultFgColor"/>\n'
             '  </style:style>'
         )
+
         mappings = {
             'DefaultBgColor': DEFAULT_PLOTLINE_COLOR,
             'DefaultFgColor': DEFAULT_TEXT_COLOR,

commit 916d2b7e2931cef2da69b53fe6c5c7c85a402496

Fixed a regression from 53829da.

src/nvlib/model/html/html_plot_list.py
@@ -49,7 +49,7 @@ class HtmlPlotList(HtmlReport):
                 f'td.h{plId} {{'
                 f'background: {bgColor}; '
                 f'color: {fgColor}'
-                '}}'
+                '}'
             )
 
             # Plot line node cell style.
@@ -58,7 +58,7 @@ class HtmlPlotList(HtmlReport):
                 f'border-left: 0.5em solid {bgColor}; '
                 f'background: {DEFAULT_PLOTLINE_COLOR}; '
                 f'color: {DEFAULT_TEXT_COLOR}'
-                '}}'
+                '}'
             )
         htmlText.append(
             '</style>\n'