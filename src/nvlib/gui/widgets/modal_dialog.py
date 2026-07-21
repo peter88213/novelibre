c4927cc2941c941129483be048c2371d0a8396b9
peter88213 <petert88213@gmail.com>
2026-07-21 07:02:25 +0200

    Refactored the code.

--- src/nvlib/gui/widgets/modal_dialog.py
+++ src/nvlib/gui/widgets/modal_dialog.py
@@ -20,5 +20,5 @@ class ModalDialog(tk.Toplevel):
         self.geometry(windowGeometry)
         self.grab_set()
         self.focus()
-        self.attributes('-topmost', 'true')
+        self.attributes('-topmost', True)
 
