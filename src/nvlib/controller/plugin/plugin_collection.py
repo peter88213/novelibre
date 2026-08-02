37cfb010b5858af1882b93f664c2ea6e259c1d9e
peter88213 <petert88213@gmail.com>
2026-08-02 19:59:25 +0200

    Version 5.63.0

--- src/nvlib/controller/plugin/plugin_collection.py
+++ src/nvlib/controller/plugin/plugin_collection.py
@@ -59,7 +59,7 @@ class PluginCollection(dict, SubController):
         except ValueError:
             # Set defaults for testing.
             self.majorVersion = 5
-            self.minorVersion = 62
+            self.minorVersion = 63
             self.patchlevel = 0
 
     def uninstall_plugin(self, pluginName):
